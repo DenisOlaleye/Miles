728593822ef879758302da57d543909ce8b1b78e
DenisOlaleye <denis.olaleye@gmail.com>
2022-08-01 18:14:38 +0300

    2.0

--- src/MileConversion.java
+++ src/MileConversion.java
@@ -1,19 +1,25 @@
+import java.util.Scanner;
 
 public class MileConversion 
 {
 	public static void main (String [] args)
 	{
+		Scanner input = new Scanner(System.in);
+		
 		final int INCHES = 63360; 
 		final int FEET = 5280;
 		final int YARDS = 1760;
-		double miles = 0.7;
+		
+		System.out.print("Enter number of miles >> ");
+		double miles = input.nextDouble();
 
 		double yardsInMile = miles*YARDS;
 		double feetInMile = miles*FEET;
 		double inchesInMile = miles*INCHES;
 		
-		System.out.println(miles+ "mile(-s) is " +yardsInMile+ " yards, "
-				+feetInMile+ " feet and " +inchesInMile+ "inches.");
+		
+		System.out.println(miles+ " mile(-s) is " +yardsInMile+ " yards, "
+				+feetInMile+ " feet and " +inchesInMile+ " inches.");
 		
 	}
 }
